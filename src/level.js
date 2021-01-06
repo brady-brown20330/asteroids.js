cfcbfc45bced898047e43ae83e7996fed79886c4
brownb202 <brownb202@gmail.com>
2021-01-06 15:50:45 -0700

    ship now responds to basic movement commands

--- src/level.js
+++ src/level.js
@@ -1,19 +1,49 @@
 import { makeSprite } from "@replay/core";
 import { Ship } from "./ship";
 
-const shipX = 0;
-
 export const Level = makeSprite({
   init() {
     return {
-      shipY: 10,
+      shipY: 0,
+      shipX: 0,
     };
   },
+
+  loop({ state, device }) {
+    let { shipY, shipX } = state;
+
+    const { inputs } = device;
+
+    // shipGravity += 0.8;
+    // shipY -= shipGravity;
+    if (inputs.keysJustPressed["ArrowUp"]) {
+      shipY += 4;
+    }
+
+    if (inputs.keysJustPressed["ArrowDown"]) {
+      shipY -= 4;
+    }
+
+    if (inputs.keysJustPressed["ArrowLeft"]) {
+      shipX -= 4;
+    }
+
+    if (inputs.keysJustPressed["ArrowRight"]) {
+      shipX += 4;
+    }
+
+    return {
+      // shipGravity,
+      shipY,
+      shipX,
+    };
+  },
+
   render({ state }) {
     return [
       Ship({
         id: "ship",
-        x: shipX,
+        x: state.shipX,
         y: state.shipY,
       }),
     ];
